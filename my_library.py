d1a5c46a966a1099d767595ad90e65af699f1f31
minkyoung <hellobaemin@gmail.com>
2023-01-18 18:02:15 +0900

    23-01-18

---
+++ my_library.py
@@ -0,0 +1,7 @@
+MY_VAl = 10
+
+def printHello(a):
+    print('hello', a)
+
+def hellow():
+    return 'aaa'
---
+++ sample.py
@@ -0,0 +1,4 @@
+from my_library import printHello
+import my_library
+
+printHello(my_library.hellow())
