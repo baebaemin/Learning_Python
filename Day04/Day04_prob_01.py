b3e7001f4330d7841d5c6c33e1d514688aa29d1d
배민경 <baeminkyoung@baemingyeong-ui-MacBookPro.local>
2023-01-23 08:56:22 +0900

    230123/08:56
    .

--- Day04/Day04_prob_01.py
+++ Day04/Day04_prob_01.py
@@ -5,19 +5,17 @@ for i in (range(1, N // 2 + 1)):
     if N % i == 0:
         lst.append(i)
 
-print(*lst, N)
+print(*lst, N) # 애스터리크를 써서 리스트를 프린트하면 띄어쓰기 적용 
 
-# 애스터리크를 써서 리스트를 프린트하면 띄어쓰기 적용 
+# # 함수랑 filter()를 활용하는 방법
 
-# 함수랑 filter()를 활용하는 방법
-
-# N = int(input())
-# numbers = range(1, N // 2 + 1)
+# N = 10
+# n = range(1, N // 2 + 1)
 
 # def div_list(i):
 #     if N % i == 0:
 #         return i
 
-# result = filter(div_list, numbers)
-# print(sorted(result.append(N)))
+# rlt = filter(div_list, n)
+# print(*rlt, N)
 
