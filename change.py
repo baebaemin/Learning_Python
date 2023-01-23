4d0e51bacbaa7e1679aedf3d85bb88d7df7ea181
배민경 <hellobaemin@gmail.com>
2023-01-23 09:06:11 +0900

    test

--- change.py
+++ change.py
@@ -1,3 +1,5 @@
-x, y = 10, 20
-x, y = y, x
-print(x, y)
+number = 10
+
+for i in range(1, number + 1):  # 1 ~ 입력값 동안
+        if number % i == 0:    # 만약 입력값을 i로 나눴을 때, 나누어 떨어진다면
+            print(i, end=' ')
