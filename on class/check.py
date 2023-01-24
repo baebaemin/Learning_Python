64c67223a15c9880e995759688ecd514060aba51
배민경 <hellobaemin@gmail.com>
2023-01-25 06:48:11 +0900

    230125/06:48

---
+++ on class/check.py
@@ -0,0 +1,5 @@
+def odd2(n):
+    return n % 2 == 1
+
+def even(n):
+    return n % 2 == 0
--- temp.py
+++ temp.py
@@ -1,8 +1,17 @@
-def dic_list_sum(data):
-    total = 0
-    for i in data:
-        total += i.get('age')
-    return total
+word = 'SsaFyYabcdefgz'
+new = ''
 
-a = dic_list_sum([{'name': 'kim', 'age': 12}, {'name': 'lee', 'age': 4}])
-print(a)
+# 대문자를 소문자로 바꾸기
+# for ch in word:
+#     if 65 <= ord(ch) <= 90:
+#         new += chr(ord(ch) + ord('a') - ord('A'))
+#     else: new += ch
+# print(new)
+
+# 소문자를 대문자로 바꾸기
+
+for ch in word:
+    if 97 <= ord(ch) <= 122:
+        new += chr(ord(ch) - 32)
+    else: new += ch
+print(new)
